4629c660439d9672140fbe2f4f41049744496210
devopswithcloud <65442863+devopswithcloud@users.noreply.github.com>
2025-03-10 07:12:46 +0530

    Update 25-mail-custom.groovy

--- V2/6-Jenkins/Pipelines/25-mail-custom.groovy
+++ V2/6-Jenkins/Pipelines/25-mail-custom.groovy
@@ -1,25 +1,21 @@
-// Send a mail based on the status of the job 
-
 pipeline {
     agent any 
     stages {
         stage ('Build') {
             steps {
-                echo "Testing Mail Post job"
+                echo "Building the project"
             }
         }
     }
     post {
         always {
             script {
-                // mail bcc: '', body: 'Build is Success', cc: '', from: '', replyTo: '', subject: 'Jenkins Job Status', to: 'i27k8s10@gmail.com'
+                //mail bcc: '', body: 'Build is Success', cc: '', 
+                // from: '', replyTo: '', subject: 'Jenkins Job Status', to: 'i27k8s10@gmail.com'
+
                 def subject = "Job Status is: ${currentBuild.currentResult}"
                 def body = "Build Number is: ${currentBuild.number}\n" + "Status is: ${currentBuild.currentResult}\n" + "Job URL: ${env.BUILD_URL}"
-                mail (
-                    to 'i27k8s10@gmail.com',
-                    subject subject
-                    body body
-                    )
+                mail to: 'i27k8s10@gmail.com',subject: subject, body: body
             }
         }
     }
