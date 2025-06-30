50060d87ce3953a8f5af2245a424fb715db20ae5
raghu-kadali <raghu2.kadali@gmail.com>
2025-06-30 12:19:33 +0530

    Create BuildingBlocks.groovy

---
+++ Jenkins/BuildingBlocks.groovy
@@ -0,0 +1,21 @@
+//Bulding blocks of jenkin
+JenkinsMaster:
+  - The main server that controls and manages all jobs.
+
+BuildAgents:
+- Machines that run the actual build jobs.
+
+Jobs:
+ Tasks or workflows that define the build, test, and deploy process.
+
+Plugins:
+- Add extra features and tools to Jenkins.
+
+Build:
+Triggers - Events that start the build automatically.
+
+Workspace:
+- The folder where the code is built and tested.
+
+Artifacts: - Files created after the build, like reports or binaries.
+
